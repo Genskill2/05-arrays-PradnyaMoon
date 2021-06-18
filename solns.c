cfdeb9879930976ebb23e9113ccb19cb58da8650
PradnyaMoon <85018322+PradnyaMoon@users.noreply.github.com>
2021-06-18 19:44:51 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,23 @@
-/* Enter your solutions in this file */
 #include <stdio.h>
+int max(int array[], int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+    if (array[1] < array[i]) 
+    {
+      array[1] = array[i];
+    }
+  }
+  return array[1];
+}
+
+int main()
+{
+    int data[]={9,5,6,10,2,-3,4};
+    int n=7;
+    int a=max(data,n);
+    printf("max:%d \n",a );
+    
+    return 0;
+    
+}
