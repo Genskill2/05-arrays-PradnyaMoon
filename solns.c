85997dc49a6baea3db16a62698672046fef1b117
PradnyaMoon <85018322+PradnyaMoon@users.noreply.github.com>
2021-06-18 19:50:29 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,4 +1,4 @@
-//max value//
+//Q.1. max value//
 
 #include <stdio.h>
 int max(int array[], int n)
@@ -24,3 +24,30 @@ int main()
     
 }
 
+
+
+//Q.2. min value//
+
+#include <stdio.h>
+int min(int array[], int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+    if (array[1] > array[i]) 
+    {
+      array[1] = array[i];
+    }
+  }
+  return array[1];
+}
+
+int main()
+{
+    int data[]={9,5,6,10,2,-3,4};
+    int n=7;
+    int a=min(data,n);
+    printf("min:%d \n",a );
+    
+    return 0;
+    
+}
